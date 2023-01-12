4b3b7a2ecdb575c209cac078e9b4a92132d21a0c
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-01-12 13:52:29 -0300

    feat: Create the moment-form component validations

--- frontend/src/app/components/moment-form/moment-form.component.ts
+++ frontend/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-moment-form",
@@ -7,4 +8,29 @@ import { Component, Input } from "@angular/core";
 })
 export class MomentFormComponent {
     @Input() btnText!: string;
+
+    momentForm!: FormGroup;
+
+    //Inicializando o formulário, não usamos o construtor pois estamos utilizando o anruglar.
+    ngOnInit(): void {
+        this.momentForm = new FormGroup({
+            id: new FormControl(""),
+            title: new FormControl("", [Validators.required]),
+            description: new FormControl("", [Validators.required]),
+            image: new FormControl(""),
+        });
+    }
+
+    get title() {
+        return this.momentForm.get("title")!;
+    }
+
+    get description() {
+        return this.momentForm.get("description")!;
+    }
+
+    onSubmit() {
+        if (this.momentForm.invalid) return;
+        console.log("Send the form");
+    }
 }
